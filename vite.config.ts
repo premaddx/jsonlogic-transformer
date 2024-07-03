43152166e6d256bb0a36ea852b49b80d30590882
Prem Anand Topno <prem@cashfree.com>
2024-07-03 10:17:59 +0530

    fix(package.json): Fixing ReferenceError exports is not defined

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'JsonLogicTransformer',
-      formats: ['es', 'cjs', 'umd'],
+      formats: ['es', 'cjs'],
       fileName: (format) => `jsonlogic-transformer.${format}.js`,
     },
   },
